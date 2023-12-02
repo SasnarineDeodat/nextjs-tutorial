6320056d4bc08f0de415cc27fcd69d0587f041e3
Robbie213 <sasnarinedeodat407@gmail.com>
2023-12-02 18:41:18 -0500

    Link Component

--- app/page.js
+++ app/page.js
@@ -1,7 +1,11 @@
-export default function Home() {
+import Link from "next/link";
+export default function HomePage() {
   return (
     <div>
       <h1 className="text-7xl">Home Page</h1>
+      <Link href="/about" className="text-2xl">
+        about page
+      </Link>
     </div>
   );
 }
