c73aa0e09c40fa35dae2a45bf11b5e2591a12d2a
Robbie213 <sasnarinedeodat407@gmail.com>
2023-12-03 14:36:47 -0500

    Fetch Data

--- app/drinks/page.js
+++ app/drinks/page.js
@@ -1,4 +1,9 @@
-export default function DrinksPage() {
+const url = "https://www.thecocktaildb.com/api/json/v1/1/search.php?f=a";
+
+export default async function DrinksPage() {
+  const response = await fetch(url);
+  const data = await response.json();
+  console.log(data);
   return (
     <div>
       <h1 className="text-7xl">DrinksPage</h1>
