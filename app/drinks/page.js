e8ad1993555af0b85980ff9925897c739b910c14
Robbie213 <sasnarinedeodat407@gmail.com>
2023-12-03 15:42:26 -0500

    Drinks List - Challenge

--- app/drinks/page.js
+++ app/drinks/page.js
@@ -1,9 +1,12 @@
+import DrinksList from "../../components/DrinksList";
+
 const url = "https://www.thecocktaildb.com/api/json/v1/1/search.php?f=a";
 
 const fetchDrinks = async () => {
   await new Promise((resolve) => setTimeout(resolve, 1000));
   const response = await fetch(url);
   // throw error
+
   if (!response.ok) throw new Error("Failed to fetch drinks");
   const data = await response.json();
   return data;
@@ -11,11 +14,9 @@ const fetchDrinks = async () => {
 
 export default async function DrinksPage() {
   const data = await fetchDrinks();
-
-  console.log(data);
   return (
     <div>
-      <h1 className="text-7xl">DrinksPage</h1>
+      <DrinksList drinks={data.drinks} />
     </div>
   );
 }
