b917f69dc1a535aa5cfb4c8a20b744b2f1a752d9
Robbie213 <sasnarinedeodat407@gmail.com>
2023-12-03 15:57:25 -0500

    Single Drink - Challenge

--- app/drinks/[id]/page.js
+++ app/drinks/[id]/page.js
@@ -1,4 +1,26 @@
-export default function SingleDrinkPage({ params }) {
-  console.log(params.id);
-  return <div>single drink page</div>;
+import Link from "next/link";
+const url = "https://www.thecocktaildb.com/api/json/v1/1/lookup.php?i=";
+
+const getSingleDrink = async (id) => {
+  const res = await fetch(`${url}${id}`);
+  if (!res.ok) {
+    throw new Error("Failed to fetch a drink...");
+  }
+  return res.json();
+};
+
+export default async function SingleDrinkPage({ params }) {
+  const data = await getSingleDrink(params.id);
+  const title = data?.drinks[0]?.strDrink;
+  const imgSrc = data?.drinks[0]?.strDrinkThumb;
+  // console.log(title, imgSrc);
+
+  return (
+    <div>
+      <Link href="/drinks" className="btn btn-primary mt-8 mb-12">
+        back to drinks
+      </Link>
+      <h1 className="text-4xl mb-8">{title}</h1>
+    </div>
+  );
 }
