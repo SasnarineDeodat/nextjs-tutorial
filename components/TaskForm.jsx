89074983c8df2fc705ad8e08ea0bd87b59155681
Robbie213 <sasnarinedeodat407@gmail.com>
2023-12-07 15:51:50 -0500

    First Server Action

--- components/TaskForm.jsx
+++ components/TaskForm.jsx
@@ -1,3 +1,31 @@
+import { revalidatePath } from "next/cache";
+
+const createTask = async (formData) => {
+  "use server";
+  const content = formData.get("content");
+  await prisma.task.create({
+    data: {
+      content,
+    },
+  });
+  revalidatePath("/tasks");
+};
+
 export default function TaskForm() {
-  return <div>TaskForm</div>;
+  return (
+    <form action={createTask}>
+      <div className="join w-full">
+        <input
+          type="text"
+          className="input input-bordered join-item w-full"
+          placeholder="type here"
+          name="content"
+          required
+        />
+        <button type="submit" className="btn btn-primary join-item">
+          create task
+        </button>
+      </div>
+    </form>
+  );
 }
