e8ea089ce6ba0ff361f640ab44da654c313f42d3
Robbie213 <sasnarinedeodat407@gmail.com>
2023-12-07 14:05:44 -0500

    Prisma CRUD

--- app/prisma-example/page.js
+++ app/prisma-example/page.js
@@ -1,7 +1,30 @@
-export default function PrismaExample() {
+import prisma from "@/utils/db";
+
+const prismaHandlers = async () => {
+  await prisma.task.create({
+    data: {
+      content: "wake up",
+    },
+  });
+  const allTasks = await prisma.task.findMany({
+    orderBy: {
+      createdAt: "desc",
+    },
+  });
+  return allTasks;
+};
+export default async function PrismaExample() {
+  const tasks = await prismaHandlers();
   return (
     <div>
       <h1 className="text-7xl">PrismaExample</h1>
+      {tasks.map((task) => {
+        return (
+          <h2 key={task.id} className="text-left py-2">
+            😻{task.content}
+          </h2>
+        );
+      })}
     </div>
   );
 }
