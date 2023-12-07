79987d0a9f9359b7078228783b0645cb595671dd
Robbie213 <sasnarinedeodat407@gmail.com>
2023-12-07 18:50:51 -0500

    Edit Task - Complete

--- app/tasks/[id]/page.js
+++ app/tasks/[id]/page.js
@@ -5,11 +5,13 @@ import EditForm from "@/components/EditForm";
 export default async function SingleTaskPage({ params }) {
   const task = await getTask(params.id);
   return (
-    <div className="mb-16">
-      <Link href="/tasks" className="btn btn-accent">
-        back to tasks
-      </Link>
+    <>
+      <div className="mb-16">
+        <Link href="/tasks" className="btn btn-accent">
+          back to tasks
+        </Link>
+      </div>
       <EditForm task={task} />
-    </div>
+    </>
   );
 }
