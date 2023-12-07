426156e50b6ee5da5f8a45c8fb3f93a1342a4e2a
Robbie213 <sasnarinedeodat407@gmail.com>
2023-12-07 15:15:33 -0500

    Display Tasks - Challenge

--- app/tasks/page.js
+++ app/tasks/page.js
@@ -1,6 +1,11 @@
+import TaskForm from "../../components/TaskForm";
+import TaskList from "../../components/TaskList";
+
 export default function TasksPage() {
   return (
-    <div>
+    <div className="max-w-lg">
+      <TaskForm />
+      <TaskList />
       <h1 className="text-7xl">TasksPage</h1>
     </div>
   );
