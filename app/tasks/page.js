c8f906f8bd9c896c3cdbfce5d0c61cd483a02ec3
Robbie213 <sasnarinedeodat407@gmail.com>
2023-12-08 17:20:53 -0500

    complete

--- app/tasks/page.js
+++ app/tasks/page.js
@@ -1,6 +1,8 @@
 import TaskFormCustom from "@/components/TaskFormCustom";
 import TaskList from "@/components/TaskList";
 
+export const dynamic = "force-dynamic";
+
 export default function TasksPage() {
   return (
     <div className="max-w-lg">
